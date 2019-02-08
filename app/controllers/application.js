142a9066bfc0e32f95f97462edfba49a8df5e733
Sumeet Attree <sumeet.attree@gmail.com>
2019-02-08 19:00:27 +0530

    Table structure

---
+++ app/controllers/application.js
@@ -0,0 +1,13 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  rows: [{
+    "id": 1,
+    "name": "Game #1",
+    "state": "lost"
+  },{
+    "id": 2,
+    "name": "Game #2",
+    "state": "lost"
+  }]
+});
